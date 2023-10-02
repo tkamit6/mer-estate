7517226ea49ba686f44e4086d3ae9363c359943a
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-10-02 14:51:44 +0530

    mongo api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_PASSWORD).then(()=>{
+    console.log("Connected to server");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
